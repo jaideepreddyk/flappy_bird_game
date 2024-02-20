e1cb059878cdd4657e0d48eeec1af78cd99bfd8f
Jaideep Reddy Kommera <42896931+jaideepreddyk@users.noreply.github.com>
2024-02-20 18:57:29 -0500

    adding images

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -8,8 +8,21 @@ public class FlappyBird extends JPanel{
     int boardwidth = 360;
     int boardHeight = 640;
 
+    // Adding images
+    Image backgroundImage;
+    Image birdImg;
+    Image topPipeImg;
+    Image bottomPipeImg;
+
     FlappyBird(){
         setPreferredSize(new Dimension(boardwidth, boardHeight));
         setBackground(Color.BLUE);
+
+        // loading images
+        // getClass will get the location of the current class, then you can just do ./ when doing get resource
+        backgroundImage = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
     }
 }
